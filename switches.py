97c44c9b7f97f0ab5629974db9ca74e253a4de15
NetAcad DEVASC <changeme@example.com>
2022-05-01 14:58:19 +0000

    switches by id added

--- switches.py
+++ switches.py
@@ -1,7 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from models import Switch
 
 switches_blueprint = Blueprint('switches', __name__)
 
 @switches_blueprint.route('', methods=['GET'])
 def example() -> str:
     return 'switch endpoint test'
+
+
+@switches_blueprint.route("/detail/<id>", methods=['GET'])
+def switch_by_id(id: str) -> str:
+    if not id.isdigit():
+        return jsonify(
+            {
+                'error': 'id must be an integer'
+            }
+        ), 400
+    id=int(id)
+    switch = Switch.query.filter_by(id=id).first()
+
+    if switch is None:
+        return jsonify({'error': '404 not found'}), 404
+    return jsonify(switch.serialize())
